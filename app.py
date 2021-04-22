be39ede20b7e2ce2831dfdb603dba3e3e1bb052d
a.v.cherkashin <a.v.cherkashin@tinkoff.ru>
2021-04-22 12:36:43 +0500

    add databse

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class Articles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
 @app.route("/")
@@ -11,6 +27,25 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/create-article", methods=['POST', 'GET'])
+def add_article():
+    if request.method == "POST":
+        title = request.form["title"]
+        intro = request.form["intro"]
+        text = request.form["text"]
+
+        article = Articles(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "Случилась ошибка"
+    else:
+        return render_template("add_article.html")
 
 
 @app.route("/user/<string:name>/<int:id>")
