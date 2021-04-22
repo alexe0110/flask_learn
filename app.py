131f52c07caeae646292177cf9119c857c3fa06f
a.v.cherkashin <a.v.cherkashin@tinkoff.ru>
2021-04-22 18:48:59 +0500

    delete and update

--- app.py
+++ app.py
@@ -53,10 +53,40 @@ def posts():
     articles = Articles.query.order_by(Articles.date).all()
     return render_template("posts.html", articles=articles)
 
+
 @app.route("/posts/<int:id>")
 def post_detail(id):
     article = Articles.query.get(id)
     return render_template("posts_detail.html", article=article)
+
+
+@app.route("/posts/<int:id>/delete")
+def post_delete(id):
+    article = Articles.query.get_or_404(id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect("/posts")
+    except:
+        return "Случилась ошибка при удалении"
+    return render_template("posts_detail.html", article=article)
+
+@app.route("/posts/<int:id>/update", methods=['POST', 'GET'])
+def post_update(id):
+    article = Articles.query.get(id)
+    if request.method == "POST":
+        article.title = request.form["title"]
+        article.intro = request.form["intro"]
+        article.text = request.form["text"]
+
+        try:
+            db.session.commit()
+            return redirect("/posts")
+        except:
+            return "Случилась ошибка при редактировании"
+    else:
+        return render_template("post_update.html", article=article)
 
 if __name__ == "__main__":
     app.run(debug=True)
